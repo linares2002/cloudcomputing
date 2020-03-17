2a30a86433a6ab1143c77478b207398a3f9b0ab8
linares2002 <jemunoz@ujaen.es>
2020-03-17 11:25:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+import numpy as np
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+print("A={0}\n".format(a))
+print("B={0}\n".format(b))
 
-app = Flask(__name__)
+# suma
+suma = a + b
+print("Suma")
+print(suma)
 
+# producto escalar
+escalar = 2 * a
+print("\nProducto escalar")
+print(escalar)
 
-@app.route("/")
-def principal():
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run()
+# producto
+multiplica = np.dot(a, b)
+print("\nProducto de matrices")
+print(multiplica)
